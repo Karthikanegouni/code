b728faf5cc566978d79f15972e6f3285aacb32c7
Karthik Goud <karthikanegouni@gmail.com>
2023-05-20 21:04:06 +0530

    python

---
+++ python/morsecode.py
@@ -0,0 +1,65 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+	cipher = ''
+	for letter in message:
+		if letter != ' ':
+			cipher += MORSE_CODE_DICT[letter] + ' '
+		else:
+			cipher += ' '
+	return cipher
+
+def decrypt(message):
+	message += ' '
+	decipher = ''
+	citext = ''
+	for letter in message:
+		if (letter != ' '):
+			i = 0
+			citext += letter
+		else:
+			i += 1
+			if i == 2 :
+				decipher += ' '
+			else:
+				decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+				.values()).index(citext)]
+				citext = ''
+
+	return decipher
+\
+def main():
+	print("Enter 1 to Encrypt Text")
+	print("Enter 2 to Decrypt Code")
+	print("Enter 3 to exit")
+	while(True):
+		i = int(input())
+		if(i==1):
+			message = input()
+			result = encrypt(message.upper())
+			print (result)
+		elif(i==2):
+			enc = input()
+			result = decrypt(enc)
+			print (result)
+		elif(i==3):
+			exit(0)
+		else:
+			print("Choose correct option")
+
+if __name__ == '__main__':
+	main()
